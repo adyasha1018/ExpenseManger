ade7858b9b1a3e393a1e3c106223967fce04e6aa
Adyasha <amishra@cuurios.com>
2023-07-04 16:13:07 +0200

    Initial set up

---
+++ money-matters/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'This is an app to deal with expenses';
+}
