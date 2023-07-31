b45c8ebb39d17c49b7f0583bca79c1e162c033ea
Adyasha <amishra@cuurios.com>
2023-07-31 14:47:33 +0200

    dashboard page,accounts component added with routing

--- money-matters/src/app/app.component.ts
+++ money-matters/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'This is an app to deal with expenses';
+  title = 'Dashboard';
+  toggleMenu:boolean = false;
 }
